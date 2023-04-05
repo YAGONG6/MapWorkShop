01c2910ef4e592be245c435bd19ec2a482cb66a2
YanGuang Gong <yanguang.gong@stonybrook.edu>
2023-04-04 21:22:19 -0400

    wegds

--- index.js
+++ index.js
@@ -31,9 +31,9 @@ app.use(express.urlencoded({ extended: true }));
 
 app.get('/count',  async (req, res) => {
  
-  const data = await Counter.findOne({_id:id});
-  console.log(data)
-  res.status(200).json({ counter: data.counter  });
+  //const data = await Counter.findOne({_id:id});
+  //console.log(data)
+  res.status(200).json({ counter: 5  });
 });
 app.get('/inc',  async (req, res) => {
   const data = await Counter.findOneAndUpdate({_id:id},{ $inc: { counter: 1 } }, { returnDocument: "after" });
