8592f0599f39b6919f074998026a5638a0f0b8c6
YanGuang Gong <yanguang.gong@stonybrook.edu>
2023-04-06 18:44:56 -0400

    wsg

--- client/src/CounterButton.js
+++ client/src/CounterButton.js
@@ -3,11 +3,15 @@ import axios from 'axios';
 
 function CountButton() {
   const [count, setCount] = useState(0);
-  const backendUrl = `https://map-work-shop.herokuapp.com`;
+  //const backendUrl = `https://map-work-shop.herokuapp.com`;
   async function handleClick () {
-    const response = await axios.get(`${backendUrl}/api/inc`);
+    const response = await axios.post(`https://map-work-shop.herokuapp.com/MapWorkShop/users/signin`, {
+      email: "yanguang.gong@stonybrook.edu",
+      password: "123456",
+      username: "1fweg3"
+    });
     console.log(response.data);
-    setCount(count + 1);
+    //setCount(count + 1);
   }
   useEffect(() => {
     // async function fetchCount() {
